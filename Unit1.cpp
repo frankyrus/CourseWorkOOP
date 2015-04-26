c081c94028193baa15789cd2d25c1b6197d4a264
frankyrus <franky.rus@gmail.com>
2015-04-26 20:21:32 +0300

    saving and loading seems to be working

--- Unit1.cpp
+++ Unit1.cpp
@@ -17,5 +17,33 @@ void __fastcall TDogForm::Exit1Click(TObject *Sender)
 {
  Close();
 }
+//-------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+
+
+void __fastcall TDogForm::Save1Click(TObject *Sender)
+{ SaveDialog1 -> Title = "Save File...";                                  // заголовок окна
+  SaveDialog1 -> Filter = "Dog files (*.dat)|*.DAT";        //шаблон имени файла
+  if (SaveDialog1->Execute())
+	  dogsTree-> SaveToFile(SaveDialog1->FileName);
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TDogForm::Open1Click(TObject *Sender)
+{
+  OpenDialog1 -> Title = "Open File...";                                  // заголовок окна
+  OpenDialog1 -> Filter = "Dog files (*.dat)|*.DAT";        //шаблон имени файла
+  if (OpenDialog1->Execute())
+	if (FileExists (OpenDialog1->FileName))
+	  dogsTree -> LoadFromFile(OpenDialog1->FileName);
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TDogForm::Cleartree1Click(TObject *Sender)
+{
+	dogsTree -> Items -> Clear();
+}
 //---------------------------------------------------------------------------
 
