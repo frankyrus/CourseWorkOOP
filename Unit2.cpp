978df1409a9173d30916d61d108463b6cad43ca5
frankyrus <franky.rus@gmail.com>
2015-05-04 21:03:37 +0300

    saving dogs and loading images... trying again

---
+++ Unit2.cpp
@@ -0,0 +1,50 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Unit2.h"
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TdogeAdd *dogeAdd;
+//---------------------------------------------------------------------------
+__fastcall TdogeAdd::TdogeAdd(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+
+void __fastcall TdogeAdd::Button1Click(TObject *Sender)
+{
+	if (ComboBox1->ItemIndex==0) {
+	DogForm->dogsTree->Items->AddChild(DogForm->dogsTree->Items->Item[0],Edit1->Text);
+	}
+	if (ComboBox1->ItemIndex==1) {
+	DogForm->dogsTree->Items->AddChild(DogForm->dogsTree->Items->Item[1],Edit1->Text);
+	}
+	if (ComboBox1->ItemIndex==2) {
+	DogForm->dogsTree->Items->AddChild(DogForm->dogsTree->Items->Item[2],Edit1->Text);
+	}
+	Memo1->Lines->Insert(0,"Продолжительность жизни: " + Edit2->Text);
+	Memo1->Lines->Insert(1,"Рост (в холке): " + Edit3->Text);
+	Memo1->Lines->Insert(2,"Вес: " + Edit4->Text);
+	Memo1->Lines->Insert(3,"Вес: " + Edit5->Text);
+	Memo1->Lines->SaveToFile(Edit1->Text);
+	Memo1->Clear();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TdogeAdd::Button2Click(TObject *Sender)
+{
+  DogForm->OpenDialog1 -> Title = "Open File...";
+  DogForm->OpenDialog1 -> Filter = "Image files (*.jpg)|*.JPG";
+  if (DogForm->OpenDialog1->Execute())
+	if (FileExists (DogForm->OpenDialog1->FileName))
+	  Image1->Picture->LoadFromFile(DogForm->OpenDialog1->FileName) ;
+}
+//---------------------------------------------------------------------------
+
